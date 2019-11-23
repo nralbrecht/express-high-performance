28e7ec928d13da043dfbedfe98bc8c18f1aca9a7
nralbrecht <nralbrecht@web.de>
2019-11-23 16:14:09 +0100

    Clean up mongoose configuration

--- models/MongoStarter.js
+++ models/MongoStarter.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+const MONGO_CONNECTION_STRING = "mongodb://localhost:27017/supermongo";
+
 function start() {
 
     const options = {
@@ -8,12 +10,14 @@ function start() {
         reconnectInterval: 500, // Reconnect every 500ms
         poolSize: 10, // Maintain up to 10 socket connections
         // If not connected, return errors immediately rather than waiting for reconnect
-        bufferMaxEntries: 0
+        bufferMaxEntries: 0,
+        useNewUrlParser: true, // current URL string parser is deprecated
+        useUnifiedTopology: true // current Server Discovery and Monitoring engine is deprecated
     };
 
     const connectWithRetry = () => {
         console.log('MongoDB connection with retry')
-        mongoose.connect("mongodb://localhost:27017/supermongo", options).then(()=>{
+        mongoose.connect(MONGO_CONNECTION_STRING, options).then(()=>{
             console.log('MongoDB is connected');
         }).catch(err=>{
             console.log('MongoDB connection unsuccessful, retry after 5 seconds.');
@@ -25,5 +29,3 @@ function start() {
 }
 
 export default start;
-
-
